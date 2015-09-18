54dec7948cc5e8cb6ba35960e93425e090cfe47c
Denis Timofeev (work) <nakolenke@gmail.com>
2015-09-18 10:27:50 +0300

    Remove stack_cache folder from repository

--- bitrix/stack_cache/MYSQL/currency_rate/61/617ce81921b8be705ca13e5be3992c16.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001442485743';
-$dateexpire = '001442496543';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:4:{s:22:"C_R_2015-09-18_BYR_RUB";a:4:{s:11:"curFromRate";d:39.43999999999999772626324556767940521240234375;s:14:"curFromRateCnt";i:10000;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-09-18_EUR_RUB";a:4:{s:11:"curFromRate";d:62.77000000000000312638803734444081783294677734375;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-09-18_UAH_RUB";a:4:{s:11:"curFromRate";d:24.530000000000001136868377216160297393798828125;s:14:"curFromRateCnt";i:10;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}s:22:"C_R_2015-09-18_USD_RUB";a:4:{s:11:"curFromRate";d:57.3900000000000005684341886080801486968994140625;s:14:"curFromRateCnt";i:1;s:9:"curToRate";d:1;s:12:"curToRateCnt";i:1;}}}';
-return true;
-?>
