85910f210a42a02b996777ece6062e90d8deea53
timofeev-denis <nakolenke@gmail.com>
2016-02-27 18:54:47 +0300

    Add warehouse address on Contacts page

--- about/contacts/index.php
+++ about/contacts/index.php
@@ -3,12 +3,23 @@ $APPLICATION->SetTitle("Контакты");
 ?><div class="row">
 	<div class="col-xs-12">
 		<p>
+		</p>
+		<h4>Офис</h4>
  <b>Телефон:</b>&nbsp;+7&nbsp;(495)&nbsp;247-55-65<br>
  <b>Email:</b>&nbsp;<a href="mailto:sales@decotec.ru?subject=Письмо с сайта decotec.ru">sales@decotec.ru</a><br>
  <b>Адрес:</b> г. Москва, Волоколамское ш., д. 97
+		<p>
 		</p>
 		 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2240.8328204917157!2d37.40012799999999!3d55.830861000000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQ5JzUxLjEiTiAzN8KwMjQnMDAuNSJF!5e0!3m2!1sen!2sru!4v1443089996268&hl=ru-RU" width="640" height="490" frameborder="0" style="border:0" allowfullscreen></iframe><br>
- <small><a href="https://maps.google.com/maps?ll=55.830506,37.399742&z=16&t=m&hl=ru-RU&gl=RU&mapclient=embed&q=%D0%92%D0%BE%D0%BB%D0%BE%D0%BA%D0%BE%D0%BB%D0%B0%D0%BC%D1%81%D0%BA%D0%BE%D0%B5%20%D1%88%D0%BE%D1%81%D1%81%D0%B5%2C%2097" style="color:#0000FF;text-align:left" target="_blank">Просмотреть увеличенную карту</a></small>
+ <br>
+ <br>
+		<p>
+		</p>
+		<h4>Склад</h4>
+ <b>Адрес:</b> Московская область, г. Химки, скр. Сходня, ул. Некрасова, д. 2, склад №3
+		<p>
+		</p>
+		 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2234.336986618211!2d37.27523831623914!3d55.943530984732575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDU2JzM2LjciTiAzN8KwMTYnMzguNyJF!5e0!3m2!1sen!2s!4v1455987478367" width="640" height="490" frameborder="0" style="border:0" allowfullscreen></iframe>
 	</div>
 </div>
  <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php")?>
