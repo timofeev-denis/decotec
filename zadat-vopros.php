53dca239af87c947cae9a077fb85070cdc347980
timofeev-denis <nakolenke@gmail.com>
2016-04-30 20:04:53 +0300

    Add 'Contacts' link to the main menu. Remove excess title on 'Ask a question' page. Remove some unnecessary text

--- zadat-vopros.php
+++ zadat-vopros.php
@@ -1,16 +1,15 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Задать вопрос");
-?><h2>Задать вопрос</h2>
-		 <?$APPLICATION->IncludeComponent(
+?>		 <?$APPLICATION->IncludeComponent(
 	"bitrix:main.feedback",
 	"eshop_adapt",
 	Array(
-		"USE_CAPTCHA" => "Y",
-		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"COMPONENT_TEMPLATE" => "eshop_adapt",
 		"EMAIL_TO" => "ishop@decotec.ru",
-		"REQUIRED_FIELDS" => array(),
 		"EVENT_MESSAGE_ID" => array(),
-		"COMPONENT_TEMPLATE" => "eshop_adapt"
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array(),
+		"USE_CAPTCHA" => "Y"
 	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
